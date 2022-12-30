cd784fb60542657e9fd9143dec2f28d975cf70b9
tandina <touretmc@protonmail.com>
2022-12-30 17:47:32 +0100

    correction rechercge tag composé pour foreach

--- script/store.js
+++ script/store.js
@@ -6,12 +6,127 @@ let store = [
   [], // utensils
 ];
 
+let view = [];
+
 // for re render notification
+// ingredient, device, utensil, tags
 let notify = [false, false, false];
 
 let IngredientTags = [];
 let DeviceTags = [];
 let UtensilsTags = [];
+
+export const setView = (recipes) => {
+  view = recipes;
+};
+export const getView = () => {
+  return view;
+};
+
+export const filterView = (view, where) => {
+  // ingredient
+  const filterIngredientView = (view) => {
+    let ret = [...getTag(where)];
+    let _store = getStore(where);
+    let ingredientView = [];
+
+    for (let i = 0; i < view.length; i++) {
+      for (let y = 0; y < view[i].ingredients.length; y++) {
+        ingredientView.push(view[i].ingredients[y].ingredient);
+      }
+    }
+    for (let i = 0; i < _store[0].length; i++) {
+      if (!ingredientView.includes(_store[0][i].name))
+        ret.push(_store[0][i].name);
+    }
+    return ret;
+  };
+
+  // device
+  const filterDeviceView = (view) => {
+    let _store = getStore(where);
+    let deviceView = [];
+    let ret = [...getTag(where)];
+
+    for (let i = 0; i < view.length; i++) {
+      deviceView.push(view[i].appliance);
+    }
+    for (let i = 0; i < _store[0].length; i++) {
+      if (!deviceView.includes(_store[0][i].name)) ret.push(_store[0][i].name);
+    }
+    return ret;
+  };
+
+  // utensils
+  const filterUtensilsView = (view) => {
+    let ret = [...getTag(where)];
+    let _store = getStore(where);
+    let utensilsView = [];
+
+    for (let i = 0; i < view.length; i++) {
+      for (let y = 0; y < view[i].ustensils.length; y++) {
+        utensilsView.push(view[i].ustensils[y]);
+      }
+    }
+    for (let i = 0; i < _store[0].length; i++) {
+      if (!utensilsView.includes(_store[0][i].name))
+        ret.push(_store[0][i].name);
+    }
+    return ret;
+  };
+  switch (where) {
+    case "INGREDIENT":
+      return filterIngredientView(view);
+    case "DEVICE":
+      return filterDeviceView(view);
+    case "UTENSILS":
+      return filterUtensilsView(view);
+    default:
+      return;
+  }
+};
+
+class _notify {
+  _value = [];
+  constructor(value) {
+    this._value = value;
+  }
+
+  setNotify(where, what) {
+    switch (where) {
+      case "INGREDIENT":
+        this._value[0] = what;
+        return;
+      case "DEVICE":
+        this._value[1] = what;
+        return;
+      case "UTENSILS":
+        this._value[2] = what;
+        return;
+      default:
+        return;
+    }
+  }
+
+  getNotify(where) {
+    switch (where) {
+      case "INGREDIENT":
+        return this._value[0];
+      case "DEVICE":
+        return this._value[1];
+      case "UTENSILS":
+        return this._value[2];
+      default:
+        return;
+    }
+  }
+
+  getAll() {
+    return this._value;
+  }
+}
+
+export const pendingTagRender = new _notify([false, false, false]);
 
 export const getNotify = (where) => {
   switch (where) {
@@ -88,19 +203,27 @@ export const addTag = (where, tag) => {
 };
 
 export const removeTag = (where, index) => {
+  let src;
+  let newArr = [];
   switch (where) {
     case "INGREDIENT":
-      IngredientTags = IngredientTags.filter((_, _index) => _index != index);
+      src = IngredientTags;
       break;
     case "DEVICE":
-      DeviceTags = DeviceTags.filter((_, _index) => _index != index);
+      src = DeviceTags;
       break;
     case "UTENSILS":
-      UtensilsTags = UtensilsTags.filter((_, _index) => _index != index);
+      src = UtensilsTags;
       break;
     default:
       return;
   }
+  for (let i = 0; i < src.length; i++) {
+    if (i != index) newArr.push(src[i]);
+  }
+  if (where == "INGREDIENT") IngredientTags = newArr;
+  else if (where == "DEVICE") DeviceTags = newArr;
+  else UtensilsTags = newArr;
 };
 
 export const getTag = (where) => {
@@ -131,7 +254,7 @@ const reRender = (value, color) => {
   removeTag(findWhere[where], index);
 
   searchBarTags();
-  setNotify(findWhere[where], true);
+  pendingTagRender.setNotify(findWhere[where], true);
 };
 
 const searchBarTags = () => {
@@ -147,7 +270,9 @@ const searchBarTags = () => {
     close.innerHTML += `<img src=images/close.svg alt="close-button">`;
     button.innerHTML += `<div class="tag-description"><p>${value}</p></div>`;
     button.appendChild(close);
-    close.addEventListener("click", () => reRender(value, color));
+    close.addEventListener("click", () => {
+      reRender(value, color);
+    });
     container.appendChild(button);
   };
 
@@ -177,13 +302,13 @@ const searchBarTags = () => {
     if (!where.length) continue;
 
     // render tag with selected color
-    where.forEach((value, index) => {
-      renderOneTag(color, value, index);
-    });
+    for (let index = 0; index < where.length; index++) {
+      renderOneTag(color, where[index], index);
+    }
   }
 };
 
-export const renderTags = (query, base, tagName, onClick, regex) => {
+export const renderTags = async (query, base, tagName, onClick, regex) => {
   if (!query) return;
 
   query.innerHTML = "";
@@ -191,21 +316,27 @@ export const renderTags = (query, base, tagName, onClick, regex) => {
     return regex.includes(check);
   };
 
-  base = base.filter((value) => !isInRegex(value.name, regex));
-  base.forEach((value, index) => {
+  // remove selected tags from tags base
+  let tmp = [];
+  for (let i = 0; i < base.length; i++) {
+    if (!isInRegex(base[i].name, regex)) tmp.push(base[i]);
+  }
+  base = tmp;
+
+  for (let index = 0; index < base.length; index++) {
     query.innerHTML += `
         <div class="tagLabel" id=${tagName}-tag-wrapper${index}>
-                      <p class="tagText"><span id=${tagName}-tag${index}></span>${value.name}</p>
+                      <p class="tagText"><span id=${tagName}-tag${index}></span>${base[index].name}</p>
         </div>
                       `;
-  });
+  }
 
-  base.forEach((value, index) => {
+  for (let index = 0; index < base.length; index++) {
     document
       .getElementById(`${tagName}-tag-wrapper${index}`)
-      .addEventListener("click", () => {
-        onClick(value.name);
+      .addEventListener("click", async () => {
+        onClick(base[index].name);
         searchBarTags();
       });
-  });
+  }
 };
